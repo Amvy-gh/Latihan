515692737df255a5deb9739b8e22b83ab3dcee9b
Amvy-gh <anjesmanik@gmail.com>
2025-01-05 17:16:44 +0700

    1

---
+++ js/latihan_1.js
@@ -0,0 +1,21 @@
+// Buatlah program yang meminta input usia dan memeriksa apakah seseorang sudah dewasa (usia >= 18) atau belum.
+
+let orang = {
+    nama :"Anjes Bermana",
+    usia : 18
+}
+
+if(orang.usia >= 18){
+    console.log(orang.nama + " sudah Dewasa")
+} else{ 
+    console.log(orang.nama + " masih Bocah")
+}
+
+// Buatlah fungsi yang menerima dua angka sebagai parameter dan menampilkan hasil perkalian kedua angka tersebut.
+
+let buah = 1;
+let apel = 2;
+
+let hasil = (buah*apel);
+
+console.log ("Hasil perkaliannya ialah " + hasil);
