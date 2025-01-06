dd800b767c9f252c0f6aa90a876064c6072d4865
Amvy-gh <anjesmanik@gmail.com>
2025-01-06 23:17:19 +0700

    4

--- js/latihan_3.js
+++ js/latihan_3.js
@@ -1,2 +1,9 @@
 // Pengelolaan Alur Program Lanjuta
 
+//Fungsi Anonim
+setTimeout(function() {
+    console.log("Ini fungsi anonim setelah 2 detik");
+}, 2000);
+
+const tambah = (a,b) => a + b;
+console.log(tambah(5, 10));
