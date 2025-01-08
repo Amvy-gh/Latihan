399f1a72a7a16b506bf3eae8d762100de1471ea0
Amvy-gh <anjesmanik@gmail.com>
2025-01-08 22:45:29 +0700

    5

--- js/latihan_3.js
+++ js/latihan_3.js
@@ -1,5 +1,3 @@
-// Pengelolaan Alur Program Lanjuta
-
 //Fungsi Anonim
 setTimeout(function() {
     console.log("Ini fungsi anonim setelah 2 detik");
@@ -7,3 +5,52 @@ setTimeout(function() {
 
 const tambah = (a,b) => a + b;
 console.log(tambah(5, 10));
+
+//error handling
+try {
+    let result = 10 / 0;
+    console.log("result: ", result); //infinity
+    throw new Error("Error terjadi");
+} catch (error) {
+    console.error("Terjadi error: ", error.message);
+} finally {
+    console.log("Kode ini akan dijalankan selalu");
+}
+
+//promises
+let fetchData = new Promise((resolve, reject) => {
+    let success = true; 
+    if (success) {
+        resolve("Data berhasil diambil");
+    } else {
+        reject(new Error("Data gagal diambil"));
+    }
+});
+
+fetchData
+    .then(data => console.log(data))
+    .catch(error => console.error(error.message));
+
+//async/await
+async function fetchData1(){
+    try {
+        let response  = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Data berhasil diambil");
+            }, 2000);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+fetchData1();
+
+//short circuiting
+let isLoggedIn = true;
+let user = isLoggedIn && "Welcome, User!";
+console.log(user); // Output: Welcome, User!
+
+let isAdmin = false;
+let access = isAdmin || "Access Denied";
+console.log(access); // Output: Access Denied
