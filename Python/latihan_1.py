94558d1b87bb5cb7dfb371a3b2ad981a5e2279fc
Amvy-gh <anjesmanik@gmail.com>
2025-01-09 23:57:13 +0700

    8

---
+++ Python/latihan_1.py
@@ -0,0 +1,13 @@
+nama = "Anjes"
+umur = 20
+tinggi = 170.5
+is_student = True
+
+a = 10
+b = 3
+print(a + b)  # Penjumlahan
+print(a - b)  # Pengurangan
+print(a * b)  # Perkalian
+print(a / b)  # Pembagian
+print(a % b)  # Modulus
+print(a ** b) # Pangkat
