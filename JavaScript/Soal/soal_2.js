3f631e25a51e80589c1247eb176fd9058e336ed4
Amvy-gh <anjesmanik@gmail.com>
2025-01-25 23:47:55 +0700

    13

---
+++ JavaScript/Soal/soal_2.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukkan nama Anda: ", (nama) => {
+  rl.question("Masukkan asal kota Anda: ", (kota) => {
+    console.log(`Halo, nama saya ${nama} dan saya berasal dari kota ${kota}.`);
+    rl.close();
+  });
+});
