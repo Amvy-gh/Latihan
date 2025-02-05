3d6dc248b137ee503727c43b3cefa05d58677465
Amvy-gh <anjesmanik@gmail.com>
2025-02-05 21:03:16 +0700

    32

---
+++ C++/latihan_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nilai_a, nilai_b, sum;
+
+    cout<<"Masukkan nilai a = ";
+    cin>>nilai_a;
+    cout<<"Masukkan nilai b = ";
+    cin>>nilai_b;
+
+    sum = nilai_a + nilai_b;
+    cout<<"Total kedua nilai tersebut ialah "<<sum;
+}
