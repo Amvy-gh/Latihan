c9133bca567de0818f715931d0ff7fe828e0919f
Amvy-gh <anjesmanik@gmail.com>
2025-03-22 00:36:56 +0700

    74

--- Kotlin/latihan_1.kt
+++ Kotlin/latihan_1.kt
@@ -1,3 +1,14 @@
+fun greetUser(name: String) {
+    println("Hello, $name! Welcome to Kotlin programming.")
+}
+
+fun addNumbers(a: Int, b: Int): Int {
+    return a + b
+}
+
 fun main() {
     println("Hello, Kotlin di VS Code!")
+    greetUser("Anjes")
+    val sum = addNumbers(5, 10)
+    println("The sum of 5 and 10 is $sum")
 }
