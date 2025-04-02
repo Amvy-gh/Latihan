80fb08f42de420d6f5c9337b9e561d9f2f151074
Amvy-gh <anjesmanik@gmail.com>
2025-04-02 23:58:09 +0700

    88

--- Kotlin/latihan_3.kt
+++ Kotlin/latihan_3.kt
@@ -49,18 +49,35 @@
 // }
 
 
-data class Karyawan(val name: String, val age: Int, val gaji: Double)
-fun hitungGaji(gaji: Double, bonus: Double = 0.0): Double{
-    return gaji + bonus;
+// data class Karyawan(val name: String, val age: Int, val gaji: Double)
+// fun hitungGaji(gaji: Double, bonus: Double = 0.0): Double{
+//     return gaji + bonus;
+// }
+
+// fun main() {
+//     val karyawan1 = Karyawan("Anjes", 25, 5000000.0)
+//     val karyawan2 = Karyawan("Ryla", 23, 6000000.0)
+
+//     val totalGaji1 = hitungGaji(karyawan1.gaji, 500000.0) // Bonus 500 ribu
+//     val totalGaji2 = hitungGaji(karyawan2.gaji) // Tidak ada bonus
+
+//     println("${karyawan1.name} mendapatkan total gaji: Rp$totalGaji1")
+//     println("${karyawan2.name} mendapatkan total gaji: Rp$totalGaji2")
+// }
+
+
+sealed class Result
+data class Success(val data: String) : Result()
+data class Error(val error: String) : Result()
+
+fun fetchData(): Result {
+    // Misalnya kita gagal mendapatkan data
+    return Success("Network Success")
 }
 
 fun main() {
-    val karyawan1 = Karyawan("Anjes", 25, 5000000.0)
-    val karyawan2 = Karyawan("Ryla", 23, 6000000.0)
-
-    val totalGaji1 = hitungGaji(karyawan1.gaji, 500000.0) // Bonus 500 ribu
-    val totalGaji2 = hitungGaji(karyawan2.gaji) // Tidak ada bonus
-
-    println("${karyawan1.name} mendapatkan total gaji: Rp$totalGaji1")
-    println("${karyawan2.name} mendapatkan total gaji: Rp$totalGaji2")
+    when (val result = fetchData()) {
+        is Success -> println("Data: ${result.data}")
+        is Error -> println("Error: ${result.error}")
+    }
 }
